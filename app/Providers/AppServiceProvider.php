db240e6b3099cb3cbbd1a931f8862acac64550e0
Akinyemi Taiwo <karrim4real@gmail.com>
2022-05-22 06:05:42 -0700

    some file added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         $categorys = Category::all();
         view()->share('categorys', $categorys);
             if(Auth::user()){
