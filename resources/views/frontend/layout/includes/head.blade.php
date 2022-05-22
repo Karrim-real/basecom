a39fa859414311990ca2af727279cf7ba4862718
Akinyemi Taiwo <karrim4real@gmail.com>
2022-05-22 06:01:03 -0700

    some file added

--- resources/views/frontend/layout/includes/head.blade.php
+++ resources/views/frontend/layout/includes/head.blade.php
@@ -10,17 +10,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/assets/img/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{assets('/assets/img/favicon.ico')}}">
 
     <!-- CSS
     ========================= -->
 
 
     <!-- Plugins CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    <link rel="stylesheet" href="{{ assets('assets/css/plugins.css') }}">
 
     <!-- Main Style CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ assets('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ assets('assets/css/custom.css') }}">
 
 </head>
