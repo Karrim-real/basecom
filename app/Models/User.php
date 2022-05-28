abafb2d9a37c524c68e5e43a9656b05023470da8
Akinyemi Taiwo <karrim4real@gmail.com>
2022-05-28 16:40:16 -0700

    some files

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,8 +46,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function VerifyAcc()
-    {
-        return $this->hasOne(VerifyAccount::class);
-    }
 }
